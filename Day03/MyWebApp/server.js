756e7d092e8b748ecc528cb7dd61b4df13b9d9ac
Prabhu <iamprabhu@yahoo.com>
2015-02-07 12:27:45 +0530

    Day03

---
+++ Day03/MyWebApp/server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules/angular"));
+
+app.post("/GeographicServer",function(request,response){
+	var params = request.query;
+	var country = params.country;
+	var output = { capital : "Not available in DB",
+				 	continent : "Not available in DB"
+				 };
+	if(country == "India"){
+		output = {
+			capital : "New Delhi",
+			continent : "Asia"
+		};
+	}
+	else if(country == "UK"){
+		output = {
+			capital : "London",
+			continent : "Europe"	
+		};
+	}
+	response.end(JSON.stringify(output));			 
+});
+app.get("/",function(request,response){
+	response.end("Hello World");
+});
+app.listen(8080);
+console.log("Server is running now");
