6fffd6ee2b2d9c32ad118cde97dd07c443b93809
Prabhu <iamprabhu@yahoo.com>
2015-02-09 11:06:36 +0530

    Day04

---
+++ Day04/Lab01.js
@@ -0,0 +1,13 @@
+var List = require("./lists");
+var EvenList = require("./evenlists");
+
+List.create([1,2,3,4,5]);
+List.add(4);
+List.remove(6);
+List.size(); //Print 7
+
+var el1 = new EvenList([2,4,6,8]);
+el1.add(10);
+el1.add(11);
+el1.add(12);
+el1.size(); //Print 5
