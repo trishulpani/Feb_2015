f31d5835e1c845d1518841e82a491330d425b436
Prabhu <iamprabhu@yahoo.com>
2015-02-10 17:27:01 +0530

    Day05

--- Day05/MyApp/server.js
+++ Day05/MyApp/server.js
@@ -1,5 +1,15 @@
 var express = require("express");
+var session = require("express-session");
 var app = express();
+var MemoryStore = session.MemoryStore;
+app.use(session({
+    store: new MemoryStore(),
+    secret: 'somekey',
+    key: 'WTH',
+	resave: true,
+    saveUninitialized: true
+}));
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -7,21 +17,31 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-app.use(express.static(__dirname + "/public"));
 require("./app/routes")(app);
 
 app.get("/",function(request,response){
-	response.sendFile("index.html");
+	response.sendFile(__dirname + "/public/index.html");
 });
-app.post("/Login",function(request,response){
+app.get("/home.html",function(request,response){
+	if(request.session.loggedIn != "yes")
+		response.redirect("/");
+	else
+		response.sendFile(__dirname + "/public/home.html");	
+});
+app.post("/Logout",function(request,response){
+	console.log(request.session);
+	request.session.destroy();
+	response.redirect("/");
+});
+app.post("/Home",function(request,response){
 	var credentials = request.body;
-	
 	 if(credentials.username == "admin" &&
 	 		credentials.password == "admin"){
 		response.sendFile(__dirname + "/public/home.html");	
+		request.session.loggedIn = "yes";
 	 }
 	 else{
- 		response.cookie("skdjfui314341","kjkjdshfdfhk",{expires:new Date(Date.now()+20000)});
+ 		response.cookie("skdjfui314341","kjkjdshfdfhk",{expires:new Date(Date.now()+10000)});
 	 	response.sendFile(__dirname + "/public/index.html");
 	 }
 });
